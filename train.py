40e55cf61a3c54b44c7fa4fbf2c2c722254a106b
SpandanPan <83960135+SpandanPan@users.noreply.github.com>
2023-09-04 12:03:11 +0530

    Update train.py

--- train.py
+++ train.py
@@ -25,7 +25,6 @@ import torchmetrics
 from torch.utils.tensorboard import SummaryWriter
 
 
-
 def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt,max_len,device):
   sos_idx=tokenizer_tgt.token_to_id('[SOS]')
   eos_idx=tokenizer_tgt.token_to_id('[EOS]')
@@ -44,5 +43,40 @@ def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt,max_l
     prob = model.project(out[:,-1])
     _,next_word = torch.max(prob,dim=1)
     decoder_input = torch.cat(
-      [decoder_input, torch.empty(1,1).type_as(source).fill_(next_word.item()).to(device)],dim=
+      [decoder_input, torch.empty(1,1).type_as(source).fill_(next_word.item()).to(device)],dim=1)
 
+    if next_word == eos_idx:
+      break
+  return decoder_input.squeeze(0)
+
+
+def get_all_sentences(ds,lang):
+  for item in ds:
+    yeild item['translation'][lang]
+
+
+def get_or_build_tokenizer(config,ds,lang):
+  tokenizer_path =Path(config['tokeinzer_file'].format(lang))
+  if not Path.exists(tokenizer_path):
+    tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+    tokenizer.pre_tokenizer =Whitespace()
+    trainer = WordLevelTrainer(special_tokens = ["[UNK]","[PAD]","[SOS]","[EOS]"],min_frequency=2)
+    tokenizer.train_from_iterator(get_all_sentences(ds,lang),trainer=trainer)
+    tokenizer.save(str(tokenizer_path))
+  else:
+    tokenizer = Tokenizer.from_file(str(tokenizer_path))
+
+return tokenizer
+
+def get_ds(config):
+
+  ds_raw = load_dataset("opus_books", f"{config['lang_src']}-{config['lang_tgt']}",split='train')
+  tokenizer_src = get_or_build_tokenizer( config,ds_raw,config['lang_src'])
+  tokenizer_tgt = get_or_build_tokenizer( config,ds_raw,config['lang_tgt'])
+
+  train_ds_size = int(0.9 - len(ds_raw))
+  val_ds_size = len(ds_raw) - train_ds_size
+  train_ds_raw,val_ds_raw = random_split(ds_raw, [train_ds_size,val_ds_size])
+
+  train_ds = BillingualDataset(train_ds_raw,tokenizer_src,tokenizer_tgt,config['lang_src']
+   
